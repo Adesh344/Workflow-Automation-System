20c0c19b38835330440187b332f9fb514aa8739d
Adesh344 <agourav311@gmail.com>
2026-03-23 04:16:00 +0530

    bug fix

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Navbar from "../components/Navbar.jsx";
-import TaskList from "../components/tasks/TaskList.jsx";
-import WorkflowList from "../components/workflows/WorkflowList.jsx";
+import Navbar from "../components/Navbarcd";
+import TaskList from "../components/tasks/TaskList";
+import WorkflowList from "../components/workflows/WorkflowList";
 import LogList from "../components/logs/LogList.jsx";
 import useTasks from "../hooks/useTasks";
 import useWorkflows from "../hooks/useWorkflows";
